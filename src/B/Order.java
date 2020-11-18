1f2203aaa1abd3b4e622fb1bfd43b3cca35cdd7b
ymhomer <ymhomer.4253@gmail.com>
2020-11-18 09:36:08 +0800

    (TKL) Update Order List

---
+++ src/B/Order.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package B;
+
+/**
+ *
+ * @author alan0
+ */
+class Order {
+    private String productName,customerName;
+    private int unitPrice,quantity,totalPrice;
+
+    public Order(String productName, String customerName, int unitPrice, int quantity, int totalPrice) {
+        this.productName = productName;
+        this.customerName = customerName;
+        this.unitPrice = unitPrice;
+        this.quantity = quantity;
+        this.totalPrice = totalPrice;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public int getUnitPrice() {
+        return unitPrice;
+    }
+
+    public void setUnitPrice(int unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public int getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(int totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+    
+    
+}
